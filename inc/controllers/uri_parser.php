f6d66796db8bc3e25716f43d61ba2c1baf48ffed
Allen Garvey <allen.garvey@gmail.com>
2016-12-13 15:35:12 -0500

    Fix logic error in regex for index route

--- inc/controllers/uri_parser.php
+++ inc/controllers/uri_parser.php
@@ -5,7 +5,7 @@
 class UriParser {
 	
 	static function isIndexRoute(string $uri, array $models): bool{
-		$indexRegex = '`^'.implode('|', array_map(function($model){ return $model::slug(); }, $models)).'/?$`';
+		$indexRegex = '`^('.implode('|', array_map(function($model){ return $model::slug(); }, $models)).')/?$`';
 		return preg_match($indexRegex, $uri);
 	}
 
