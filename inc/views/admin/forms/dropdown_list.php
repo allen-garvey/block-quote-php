ebf2f554d8c4df9a0c5b4976ce1474995416f21f
Allen Garvey <allen.garvey@gmail.com>
2016-12-14 16:11:04 -0500

    Escape input names and ids in dropdown list

--- inc/views/admin/forms/dropdown_list.php
+++ inc/views/admin/forms/dropdown_list.php
@@ -4,9 +4,9 @@
 	}
  ?>
 
-<label for="id_<?= $dropdown_name; ?>" <?php if(isset($is_required) && $is_required){ echo 'class="required"'; } ?>><?= FormHelper::labelDisplayName($dropdown_name); ?>:</label>
+<label for="id_<?= htmlentities($dropdown_name); ?>" <?php if(isset($is_required) && $is_required){ echo 'class="required"'; } ?>><?= FormHelper::labelDisplayName($dropdown_name); ?>:</label>
 <div class="related-widget-wrapper">
-    <select id="id_<?= $dropdown_name; ?>" name="<?= $dropdown_name; ?>" <?php if(isset($is_required) && $is_required){ echo 'required="required"'; } ?>>
+    <select id="id_<?= htmlentities($dropdown_name); ?>" name="<?= htmlentities($dropdown_name); ?>" <?php if(isset($is_required) && $is_required){ echo 'required="required"'; } ?>>
         <option value="" <?= FormHelper::getSelected($selected_id, ''); ?>>---------</option>
         <?php foreach($dropdown_items as $dropdown_item): ?>
         	<option value="<?= $dropdown_item['id']; ?>" <?= FormHelper::getSelected($selected_id, $dropdown_item['id']); ?>><?= $model::toHTML($dropdown_item); ?></option>
